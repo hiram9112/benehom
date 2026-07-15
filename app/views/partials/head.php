95096a4286a2de8020764310b3728902932811b8
hiram9112 <234371043+hiram9112@users.noreply.github.com>
2026-07-15 16:20:42 +0200

    chore(assets): move favicon files into public/img directory
    - Relocate all favicon and app icon files from public/ to public/img/
    - Update favicon references in head.php to use new img/ paths
    - Update icon paths in site.webmanifest to match new location

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -100,10 +100,10 @@ function bh_document_begin(array $opciones = []): void
 
     <title><?= htmlspecialchars($documentTitle, ENT_QUOTES, 'UTF-8') ?></title>
 
-    <link rel="icon" href="<?= BASE_URL ?>favicon.ico" sizes="any">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= BASE_URL ?>favicon-16x16.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= BASE_URL ?>favicon-32x32.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= BASE_URL ?>apple-touch-icon.png">
+    <link rel="icon" href="<?= BASE_URL ?>img/favicon.ico" sizes="any">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= BASE_URL ?>img/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= BASE_URL ?>img/favicon-32x32.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= BASE_URL ?>img/apple-touch-icon.png">
     <link rel="manifest" href="<?= BASE_URL ?>site.webmanifest">
     <meta name="theme-color" content="#f5ead7">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
